2e48bfb7bb9f4d8ae9cd3a5b8de5d7e1a9bb8d08
korGetAv <korichhak@gmail.com>
2023-07-14 11:44:54 +0300

    shop edits charge

--- src/js/main.js
+++ src/js/main.js
@@ -128,18 +128,72 @@ const main_init = function () {
             }
         }
         productsToolbar.addEventListener('click', closeWrapperToolbar)
+
+        const rangeInput = document.querySelectorAll(".range-input input"),
+            priceInput = document.querySelectorAll(".price-input input"),
+            range = document.querySelector(".slider .progress");
+        let priceGap = 100;
+
+        let minPrice = parseInt(priceInput[0].value),
+            maxPrice = parseInt(priceInput[1].value);
+        range.style.left = ((minPrice / rangeInput[0].max) * 100) + "%";
+        range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
+
+
+        priceInput.forEach(input => {
+            input.addEventListener("input", e => {
+                let minPrice = parseInt(priceInput[0].value),
+                    maxPrice = parseInt(priceInput[1].value);
+
+                if ((maxPrice - minPrice >= priceGap) && maxPrice <= rangeInput[1].max) {
+                    if (e.target.className === "input-min") {
+                        rangeInput[0].value = minPrice;
+                        range.style.left = ((minPrice / rangeInput[0].max) * 100) + "%";
+                    } else {
+                        rangeInput[1].value = maxPrice;
+                        range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
+                    }
+                }
+            });
+        });
+
+        rangeInput.forEach(input => {
+            input.addEventListener("input", e => {
+                let minVal = parseInt(rangeInput[0].value),
+                    maxVal = parseInt(rangeInput[1].value);
+
+                if ((maxVal - minVal) < priceGap) {
+                    if (e.target.className === "range-min") {
+                        rangeInput[0].value = maxVal - priceGap
+                    } else {
+                        rangeInput[1].value = minVal + priceGap;
+                    }
+                } else {
+                    priceInput[0].value = minVal;
+                    priceInput[1].value = maxVal;
+                    range.style.left = ((minVal / rangeInput[0].max) * 100) + "%";
+                    range.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+                }
+            });
+        });
     }
 
     if (productsList) {
+        const grid = localStorage.getItem('productsList_grid');
+        if (grid) {
+            productsList.classList.add(grid);
+        }
         const buttons = document.querySelectorAll('.products-tools__grid button');
         const toggleGrid = (e) => {
             if (e.target.classList.contains('grid')) {
                 if (productsList.classList.contains('_list')) {
                     productsList.classList.remove('_list')
+                    localStorage.removeItem('productsList_grid');
                 }
             } else {
                 if (!productsList.classList.contains('_list')) {
                     productsList.classList.add('_list')
+                    localStorage.setItem('productsList_grid', '_list');
                 }
             }
         }
