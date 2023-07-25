a794f96178ab18d778060372aac92d04789b36fb
korGetAv <korichhak@gmail.com>
2023-07-25 15:08:58 +0300

    product finish

--- docs/js/main.js
+++ docs/js/main.js
@@ -6,6 +6,9 @@ const main_init = function() {
     const toolbar = document.querySelector('.toolbar');
     const productsToolbar = document.querySelector('.products-toolbar');
     const productsList = document.querySelector('.products-list');
+    const itemView = document.querySelector('.item-view');
+    const itemAbout = document.querySelector('.item-about');
+    const itemFeature = document.querySelector('.item-feature');
 
     if (ibg.length > 0) {
         for (let i = 0; i < ibg.length; i++) {
@@ -200,6 +203,127 @@ const main_init = function() {
 
         for (const button of buttons) {
             button.addEventListener('click', toggleGrid);
+        }
+    }
+
+    if (itemView) {
+        const items = itemView.querySelectorAll('.item-view__item');
+        const image = itemView.querySelector('.item-view__image');
+        const mainImg = image.querySelector('img');
+        const delay = parseFloat(getComputedStyle(mainImg).transitionDuration) * 1000;
+
+        const changeSrc = (e) => {
+            const buttonImg = e.target.querySelector('img');
+
+            buttonImg.classList.add('_active');
+            mainImg.classList.add('_active');
+            itemView.classList.add('_active');
+
+            setTimeout(() => {
+                let tempSrc = mainImg.getAttribute('src');
+                mainImg.setAttribute('src', buttonImg.getAttribute('src'));
+                buttonImg.setAttribute('src', tempSrc);
+
+                buttonImg.classList.remove('_active');
+                mainImg.classList.remove('_active');
+                itemView.classList.remove('_active');
+            }, delay)
+        }
+
+        for (const item of items) {
+            const button = item.querySelector('button');
+            button.addEventListener('click', changeSrc)
+        }
+    }
+
+    if (itemAbout) {
+        const favorite = itemAbout.querySelector('.item-about__heading button');
+        const sizeItems = itemAbout.querySelectorAll('.item-size__item');
+        const colorItems = itemAbout.querySelectorAll('.item-color__item');
+        const itemCount = itemAbout.querySelector('.item-count');
+
+        const addFavorite = () => {
+            favorite.classList.toggle('_active');
+        }
+        favorite.addEventListener('click', addFavorite);
+
+        const changeSize = (e) => {
+            for (const sizeItem of sizeItems) {
+                if (sizeItem.classList.contains('_active')) {
+                    sizeItem.classList.remove('_active')
+                }
+            }
+            e.target.closest('.item-size__item').classList.add('_active');
+        }
+        for (const sizeItem of sizeItems) {
+            sizeItem.addEventListener('click', changeSize);
+        }
+
+        const changeColor = (e) => {
+            for (const colorItem of colorItems) {
+                if (colorItem.classList.contains('_active')) {
+                    colorItem.classList.remove('_active')
+                }
+            }
+            e.target.closest('.item-color__item').classList.add('_active');
+        }
+        for (const colorItem of colorItems) {
+            const button = colorItem.querySelector('button');
+            const color = button.dataset.color;
+            button.style.backgroundColor = color;
+
+            colorItem.addEventListener('click', changeColor);
+        }
+
+        const counter = (e) => {
+            const decr = itemCount.querySelector('.decr');
+            const out = itemCount.querySelector('.out');
+            const incr = itemCount.querySelector('.incr');
+
+            if (e.target == decr) {
+                if (Number(out.innerHTML) > 1) {
+                    out.innerHTML = Number(out.innerHTML) - 1;
+                }
+            }
+
+            if (e.target == incr) {
+                if (Number(out.innerHTML) < 100) {
+                    out.innerHTML = Number(out.innerHTML) + 1;
+                }
+            }
+        }
+        itemCount.addEventListener('click', counter);
+    }
+
+    if (itemFeature) {
+        const links = itemFeature.querySelectorAll('.item-feature__link a');
+        const sections = itemFeature.querySelectorAll('.item-feature__section');
+
+        const changeSection = (e) => {
+            e.preventDefault();
+
+            const target = e.target.closest('.item-feature__link');
+
+            for (const link of links) {
+                const target = link.closest('.item-feature__link');
+                if (target.classList.contains('_active')) {
+                    target.classList.remove('_active');
+                }
+            }
+            target.classList.add('_active');
+
+            const selector = e.target.getAttribute('href');
+            for (const section of sections) {
+                section.classList.remove('_active');
+
+                const sectionSelector = section.dataset.id;
+                if (sectionSelector === selector) {
+                    section.classList.add('_active');
+                }
+            }
+        }
+        for (const link of links) {
+            link.addEventListener('click', changeSection)
         }
     }
 }();
